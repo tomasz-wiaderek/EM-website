fdfae3598ca10aba0fc4e7699abb8f94b0c98269
t.wiaderek <t.wiaderek@gmail.com>
2022-04-22 23:37:56 +0200

    Post localhost test changes

--- script/gallery_script.js
+++ script/gallery_script.js
@@ -1,14 +1,88 @@
-import {
-  srcsModernAndTender,
-  srcsBlueAndRustic,
-  srcsGreenPatterns,
-  srcsModernVintage,
-  srcsGreenPeace,
-  srcsILoveBoho,
-  srcsSmallAndCute,
-  srcsLoftOffice
-} from '.srcs.js';
+const srcsModernAndTender = [
+  'media/modern_and_tender/1.jpg',
+  'media/modern_and_tender/2.jpg',
+  'media/modern_and_tender/3.jpg',
+  'media/modern_and_tender/4.jpg',
+  'media/modern_and_tender/5.jpg',
+  'media/modern_and_tender/6.jpg',
+  'media/modern_and_tender/7.jpg',
+]
 
+const srcsBlueAndRustic = [
+  'media/blue_and_rustic/1.jpg',
+  'media/blue_and_rustic/2.jpg',
+  'media/blue_and_rustic/4.jpg',
+  'media/blue_and_rustic/5.jpg',
+  'media/blue_and_rustic/6.jpg',
+  'media/blue_and_rustic/7.jpg',
+  'media/blue_and_rustic/8.jpg',
+  'media/blue_and_rustic/9.jpg',
+]
+
+const srcsGreenPatterns = [
+  'media/green_patterns/1.png',
+  'media/green_patterns/2.png',
+  'media/green_patterns/3.png',
+  'media/green_patterns/4.png',
+  'media/green_patterns/5.png',
+]
+
+const srcsModernVintage = [
+  'media/modern_vintage/1.png',
+  'media/modern_vintage/2.png',
+  'media/modern_vintage/3.png',
+  'media/modern_vintage/4.png',
+  'media/modern_vintage/5.png',
+  'media/modern_vintage/6.png',
+  'media/modern_vintage/7.png',
+  'media/modern_vintage/8.png',
+  'media/modern_vintage/9.png',
+  'media/modern_vintage/10.png',
+  'media/modern_vintage/11.png',
+  'media/modern_vintage/12.png',
+]
+
+const srcsGreenPeace = [
+  'media/green_peace/1.png',
+  'media/green_peace/2.png',
+  'media/green_peace/3.png',
+  'media/green_peace/4.png',
+  'media/green_peace/5.png',
+  'media/green_peace/6.png',
+  'media/green_peace/7.png',
+  'media/green_peace/8.png',
+  'media/green_peace/9.png',
+]
+
+const srcsILoveBoho = [
+  'media/i_love_boho/1.png',
+  'media/i_love_boho/2.png',
+  'media/i_love_boho/3.png',
+  'media/i_love_boho/4.png',
+  'media/i_love_boho/5.png',
+  'media/i_love_boho/6.png',
+  'media/i_love_boho/7.png',
+  'media/i_love_boho/8.png',
+  'media/i_love_boho/9.png',
+  'media/i_love_boho/10.png',
+  'media/i_love_boho/11.png',
+  'media/i_love_boho/12.png',
+  'media/i_love_boho/13.png'
+]
+
+const srcsSmallAndCute = [
+  'media/small_and_cute/1.png',
+  'media/small_and_cute/2.png',
+  'media/small_and_cute/3.png',
+  'media/small_and_cute/4.png',
+]
+
+const srcsLoftOffice = [
+  'media/loft_office/1.png',
+  'media/loft_office/2.png',
+  'media/loft_office/3.png',
+  'media/loft_office/4.png',
+]
 
 class PortfolioGallery {
   constructor(slides) {
@@ -33,11 +107,7 @@ const smallAndCute = new PortfolioGallery(srcsSmallAndCute)
 const loftOffice = new PortfolioGallery(srcsLoftOffice)
 
 
-let GalleryAgent = {}
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  GalleryAgent = {
+const GalleryAgent = {
     openGallery() {
       document.getElementById("gallery").style.display = "block";
     },
@@ -89,5 +159,3 @@ document.addEventListener('DOMContentLoaded', () => {
       this.changeDotsStyle(this.currentSlideId);
     }
   }
-
-})
